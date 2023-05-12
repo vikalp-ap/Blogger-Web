1c3834eaa5357a02fa03da6124cc64fdfaa1dcc9
Vikalp Chakravorty <95289150+vikalp-ap@users.noreply.github.com>
2023-05-12 17:13:39 +0530

    Created Project, added DB Context and done DB migration

---
+++ Data/BlogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Blogger.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blogger.Web.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+    }
+}
+
