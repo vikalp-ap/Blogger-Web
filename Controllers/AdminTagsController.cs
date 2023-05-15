dc8724a3193eba0eba20f8a9e69f99a65e6b244f
Vikalp Chakravorty <95289150+vikalp-ap@users.noreply.github.com>
2023-05-15 17:34:07 +0530

    Added CRUD operations and made controller methods async

---
+++ Controllers/AdminTagsController.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blogger.Web.Data;
+using Blogger.Web.Models.ViewModels;
+using Blogger.Web.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blogger.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly BlogDbContext blogDbContext;
+
+        public AdminTagsController(BlogDbContext blogDbContext)
+        {
+            this.blogDbContext = blogDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public async Task<IActionResult> Add(AddTagRequest addTagRequest)
+        {
+            //Mapping addTagRequest to tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+            await blogDbContext.Tags.AddAsync(tag);
+            await blogDbContext.SaveChangesAsync();
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        [ActionName("List")]
+        public async Task<IActionResult> List()
+        {
+            //Use DBContext to read the tags
+            var tags = await blogDbContext.Tags.ToListAsync();
+            return View(tags);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var tag = await blogDbContext.Tags.FirstOrDefaultAsync(x => x.Id == id);
+            if (tag != null)
+            {
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName
+                };
+                return View(editTagRequest);
+            }
+            return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditTagRequest editTagRequest)
+        {
+            var tag = new Tag
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName
+            };
+            var existingTag = await blogDbContext.Tags.FindAsync(tag.Id);
+            if (existingTag != null)
+            {
+                existingTag.Name = tag.Name;
+                existingTag.DisplayName = tag.DisplayName;
+                await blogDbContext.SaveChangesAsync();
+                //show success notification
+                return RedirectToAction("Edit", new { id = editTagRequest.Id });
+            }
+            //show error notification
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(EditTagRequest editTagRequest)
+        {
+            var tag = await blogDbContext.Tags.FindAsync(editTagRequest.Id);
+            if (tag != null)
+            {
+                blogDbContext.Tags.Remove(tag);
+                await blogDbContext.SaveChangesAsync();
+                //show success notification
+                return RedirectToAction("List");
+            }
+            //show error notification
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+        }
+    }
+}
---
+++ Models/ViewModels/AddTagRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Blogger.Web.Models.ViewModels
+{
+	public class AddTagRequest
+	{
+		public string Name { get; set; }
+        public string DisplayName { get; set; }
+
+    }
+}
+
